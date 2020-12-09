eee11fd3aa8769a2130aaa71070e83d0e7d7f0f6
Chris Gillum <cgillum@microsoft.com>
2020-12-09 13:59:56 -0800

    Implemented the following: - abandon logic - work item renewal (task activities) - split brain handling - tests for all the above

--- test/DurableTask.SqlServer.Tests/Logging/LogAssert.cs
+++ test/DurableTask.SqlServer.Tests/Logging/LogAssert.cs
@@ -55,6 +55,9 @@
         public static LogAssert CheckpointCompleted(string name) =>
             new LogAssert(306, "CheckpointCompleted", LogLevel.Information, name);
 
+        public static LogAssert DuplicateExecutionDetected(string name) =>
+            new LogAssert(307, "DuplicateExecutionDetected", LogLevel.Warning, name);
+
         public static void LogEntryCount(TestLogProvider logProvider, int expected) =>
             Assert.Equal(expected, GetLogs(logProvider).Count());
 
@@ -89,7 +92,7 @@
                 $"{asserts.Length} log entries were expected but only {actualLogs.Length} were found. Expected:{expected}Actual:{actual}");
         }
 
-        static void ValidateStructuredLogFields(LogEntry log)
+        internal static void ValidateStructuredLogFields(LogEntry log)
         {
             // All log entries are expected to have dictionary state
             var fields = log.State as IReadOnlyDictionary<string, object>;
@@ -121,6 +124,11 @@
                     Assert.True(fields.ContainsKey("InstanceId"));
                     Assert.True(fields.ContainsKey("ExecutionId"));
                     Assert.True(fields.ContainsKey("LatencyMs"));
+                    break;
+                case EventIds.DuplicateExecutionDetected:
+                    Assert.True(fields.ContainsKey("Name"));
+                    Assert.True(fields.ContainsKey("InstanceId"));
+                    Assert.True(fields.ContainsKey("ExecutionId"));
                     break;
                 default:
                     throw new ArgumentException($"Log event {log.EventId} is not known. Does it need to be added to the log validator?", nameof(log));
